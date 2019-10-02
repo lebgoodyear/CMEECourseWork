819df549edfbe032325aaca558e0caf53ba49998
Lucy Goodyear <lucy.goodyear19@imperial.ac.uk>
2019-10-02 15:34:24 +0100

    Adding First LaTeX Files

---
+++ Week1/Code/CompileLatex.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+pdflatex $1.tex
+pdflatex $1.tex
+bibtex $1
+pdflatex $1.tex
+pdflatex $1.tex
+open $1.pdf
+mv $1.pdf $2$1.pdf 
+## Cleanup
+rm *~
+rm *.aux
+rm *.dvi
+rm *.log
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
+rm *.bbl
+rm *.blg
