c5f4725e5f5648f9c80f16a4177b05441027d278
Lucy Goodyear <lucy.goodyear19@imperial.ac.uk>
2019-11-17 18:45:47 +0000

    Empty Week 5 Results

--- MiniProject/Code/DataPrep.R
+++ MiniProject/Code/DataPrep.R
@@ -20,19 +20,23 @@ qplot(x = log(crd$ResDensity), y = log(crd$TraitValue), colour = crd$ID)+
 # have a minimum of 4 datasets
 x <- as.data.frame(matrix())
 IDs <- as.data.frame(table(crd$ID))
-y <- as.character(IDs$Var1)
-for (i in (1:length(y))) {
+l <- as.character(IDs$Var1)
+for (i in (1:length(l))) {
   if (IDs[i,2] < 4) {
-    x <- rbind(x, y[i])
+    x <- rbind(x, l[i])
   }
 }
-
 # we can see from the above that are no factors with less than 4 values so
 # all can be used for fitting
-write.csv(crd, "../Data/CRatMod.csv")
 
-z <- c(crd, which(crd$ID == y[1]))
+# view some random datasets to get a feel for the data
+z <- subset(crd, crd$ID == crd$ID[1000])
 qplot(x = log(z$ResDensity), y = log(z$TraitValue)) +
   geom_point()
+
+# save subsetted data to new csv to be imported by python
+write.csv(crd, "../Data/CRatMod.csv")
+
+
 
   
---
+++ Week5/Results/README.md
@@ -0,0 +1,6 @@
+# CMEE Coursework Week 5 Results Directory
+
+*Author: Lucy Goodyear*  
+*Created: 17/11/19*
+
+This is an empty directory to which outputs are sent.
