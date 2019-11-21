f15b25d002f4a780591d379c013cb70e2005619d
Lucy Goodyear <lucy.goodyear19@imperial.ac.uk>
2019-11-21 08:02:44 +0000

    Add successful polynomial plotting

--- MiniProject/Code/Plotting.R
+++ MiniProject/Code/Plotting.R
@@ -4,14 +4,43 @@
 library(ggplot2)
 
 # define Holling function to plotting
-Holling <- function (a, q, h, x) {
-  return (a * (x ^ (q + 1)) / (1 + h * a * (x) ^ (q + 1)))
-}
+#Holling <- function (a, q, h, x) {
+#  return (a * (x ^ (q + 1)) / (1 + h * a * (x) ^ (q + 1)))
+#}
 
-e <- read.csv("../Data/crd_w_estimates")
+fits <- read.csv("../Data/crd_fits")
+fits <- subset(fits, !is.na(fits$Poly1))
+fits <- subset()
 
-qplot(x = e$ResDensity, y = e$N_TraitValue,
+#IDs <- c(unique(fits$ID))
+#pdf(paste("../Results/Explore_Plots/polyIDs.pdf"),
+#    8, 4.5, onefile = TRUE) # save all plots to one pdf
+#for (i in IDs) {
+#  p <- subset(fits, fits$ID == i) # subset and plot the data by ID
+  qplot(x = p$log_ResDensity, y = p$log_N_TraitValue,
+              xlab = "log of Resource Density", ylab = "Log of Trait Value",
+              main = paste("ID", i)) +
+          geom_point() +
+  
+ggplot(aes(x = log_ResDensity,
+           y = log_N_TraitValue),
+           data = p) +
+           xlab("log of Resource Density") + 
+           ylab("Log of Trait Value") +
+           ggtitle(paste("ID", i)) +
+      geom_point() +
+       geom_line(data = data_to_plot, 
+                 mapping = aes(x, y))
+    
+x = seq(min(p$log_ResDensity), max(p$log_ResDensity), 0.1) 
+y = p$Poly4[1] + p$Poly3[1]*x + 
+    p$Poly2[1]*x^2 + (p$Poly1[1])*x^3
+
+data_to_plot <- data.frame(x, y)
+#}
+dev.off()
+
+qplot(x = fits$log_ResDensity, y = fits$log_N_TraitValue,
       xlab = "Resource Density", ylab = "Trait Value") +
-  geom_point()
-qplot(x = e$ResDensity, y = Holling(4.314e-10, 1.3, -5.376e-10, e$ResDensity),
-xlab = "Resource Density", ylab = "Trait Value", add = T)
+  geom_point() +
+  geom_line(fits$Estimated)
--- MiniProject/Sandbox/polynomial.py
+++ MiniProject/Sandbox/polynomial.py
@@ -16,7 +16,7 @@ import numpy
 # load the data
 crd = pandas.read_csv("../Data/CRatMod.csv", index_col = 0)
 
-IDs = [39835, 39836] #crd['ID']
+IDs = crd['ID']
 
 # insert new columns into the dataframe for the final estimate of each parameter
 crd.insert(3, "Poly1", "")
@@ -26,24 +26,29 @@ crd.insert(6, "Poly4", "")
 
 # create for loop to fit Holling for all data subsets
 fits = pandas.DataFrame()
-for i in IDs:
+for i in numpy.unique(IDs):
     crd_sub = crd[crd.ID == i] # subset by each ID
     crd_sub = crd_sub.reset_index(drop=True) # reset index so can easily loop over just these elements
     v = crd_sub["log_ResDensity"].tolist()
+    # try:
     h_fit = numpy.polyfit(crd_sub["log_ResDensity"], crd_sub["log_N_TraitValue"], 3)
+    # except ValueError:
+    #     print("Errored at ID", i, " - no polynomial fit possible")
+    #     nofitIDs = notfitIDs.append(i)
+    #     pass # we can pass safely after failing IDs have been caught
     for j in range(len(crd_sub)):
         crd_sub.iloc[j,3] = h_fit[0], 
         crd_sub.iloc[j,4] = h_fit[1],
         crd_sub.iloc[j,5] = h_fit[2],
         crd_sub.iloc[j,6] = h_fit[3],
-    fits = fits.append(crd_sub.iloc[j])
-fits = fits.reindex(crd_sub.columns, axis=1)
+        fits = fits.append(crd_sub.iloc[j])
+    fits = fits.reindex(crd_sub.columns, axis=1)
+
 fits = fits.reset_index(drop=True)
 fits['ID'] = fits['ID'].astype(numpy.int64)
 
 # test plot
 import matplotlib.pylab as p
-#p.ylim((0, 0.8))
 plotpoly = numpy.poly1d(h_fit)
 p.plot(crd_sub["log_ResDensity"], crd_sub["log_N_TraitValue"], "k+")
 p.plot(crd_sub["log_ResDensity"], plotpoly(crd_sub["log_ResDensity"]), 'r')
