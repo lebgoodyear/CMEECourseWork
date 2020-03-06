bbeb51ba043221c5a1253fb2603168addc9aec5b
Lucy Goodyear <lucy.goodyear19@imperial.ac.uk>
2020-03-06 09:16:53 +0000

    Add texcount to bash script

--- MiniProject/Code/run_MiniProject.sh
+++ MiniProject/Code/run_MiniProject.sh
@@ -7,21 +7,30 @@
 
 echo "Beginning MiniProject execution"
 
+# run R data preparation script
 echo "Running data preparation script..."
 Rscript DataPrep.R
 echo "Data preparation script completed"
 
+# run python model fitting script
 echo "Running model fitting script..."
 python3 Fit_Models.py
 echo "Model fitting script completed"
 
-echo "Running plotting script..."
-Rscript Plot_Models.R
-echo "Completed plotting script"
+# run R plotting and analysis script
+echo "Running plotting and analysis script..."
+Rscript Plot_Analyse_Models.R
+echo "Completed plotting and analysis script"
 
 echo "Compiling .tex file..."
-latex MiniProject.tex
+# word count for final report
+texcount -1 -sum MiniProject.tex > MiniProject.sum
+# compile latex file to produce pdf with bibliography
+pdflatex MiniProject.tex
+bibtex MiniProject
+pdflatex MiniProject.tex
 
+# move pdf into Results folder
 mv MiniProject.pdf ../Results/MiniProject.pdf 
 
 # cleanup
@@ -29,12 +38,16 @@ rm *.log
 rm *.bbl
 rm *.xml
 rm *-blx.bib
-rm *.dvi
 rm *.aux
+rm *.sum
+rm *.blg
+rm -r __pycache__
 
+# open pdf for viewing
 open ../Results/MiniProject.pdf
 
+# print to screen so we know all has been completed
 echo "Compiling completed, pdf produced"
 echo "End of bash script — miniproject finished"
 
-# exit
+# end of script
